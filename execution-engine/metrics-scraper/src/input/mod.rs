850379e5adf6b5e7f8255a55ccb885dd061887ae
Ed Hastings <ed@casperlabs.io>
2019-06-06 14:41:11 -0700

    EE-351: complete pending @henrytill review

--- execution-engine/metrics-scraper/src/input/mod.rs
+++ execution-engine/metrics-scraper/src/input/mod.rs
@@ -12,7 +12,6 @@ fn process_line<P: Pusher<String>>(pusher: &P, line: String) -> Result<(), Accum
 
 pub(crate) struct Sink<P: Pusher<String> + 'static> {
     pusher: P,
-    //locker: StdinLock<'static>,
     flag: Arc<AtomicBool>,
 }
 
@@ -22,12 +21,17 @@ impl<P: Pusher<String>> Sink<P> {
         Sink { pusher, flag }
     }
 
-    pub fn start(&self) -> JoinHandle<()> {
+    pub fn start(&self) -> Option<JoinHandle<()>> {
+        if self.flag.load(Ordering::SeqCst) {
+            return None;
+        }
+
         self.flag.store(true, Ordering::SeqCst);
 
         let pusher = self.pusher.clone();
         let flag = self.flag.clone();
-        thread::spawn(move || {
+
+        Some(thread::spawn(move || {
             let stdin = io::stdin();
             let mut handle = stdin.lock();
 
@@ -41,7 +45,7 @@ impl<P: Pusher<String>> Sink<P> {
                     }
                 }
             }
-        })
+        }))
     }
 
     pub fn stop(&self) {
@@ -76,9 +80,38 @@ mod tests {
         let accumulator: Accumulator<String> = Accumulator::new(expected_poll_length);
         let sink = Sink::new(accumulator.clone());
 
-        let handle = sink.start();
+        let handle = sink.start().expect("should start");
         sink.stop();
 
         let _ = handle.join();
+    }
+
+    #[test]
+    fn should_start_stop_sink_multiple_times() {
+        let expected_poll_length = Duration::new(5, 0);
+        let accumulator: Accumulator<String> = Accumulator::new(expected_poll_length);
+        let sink = Sink::new(accumulator.clone());
+
+        let handle = sink.start().expect("should start");
+        sink.stop();
+        let _ = handle.join();
+
+        let handle = sink.start().expect("should start");
+        sink.stop();
+        let _ = handle.join();
+    }
+
+    #[test]
+    fn should_not_start_multiple_times() {
+        let expected_poll_length = Duration::new(5, 0);
+        let accumulator: Accumulator<String> = Accumulator::new(expected_poll_length);
+        let sink = Sink::new(accumulator.clone());
+
+        let handle1 = sink.start().expect("should start");
+        let handle2 = sink.start();
+
+        assert!(handle2.is_none(), "should not get second thread");
+        sink.stop();
+        let _ = handle1.join();
     }
 }
--- execution-engine/metrics-scraper/src/main.rs
+++ execution-engine/metrics-scraper/src/main.rs
@@ -13,6 +13,7 @@ pub mod output;
 const EXPECTED_POLL_INTERVAL_SECONDS: u64 = 5;
 const RUNNABLE_CHECK_INTERVAL_SECONDS: u64 = 3;
 const SIGINT_HANDLE_EXPECT: &str = "Error setting Ctrl-C handler";
+const SINK_START_EXPECT: &str = "Sink should start";
 
 /// Gets SIGINT handle to allow clean exit
 fn get_sigint_handle() -> Arc<AtomicBool> {
@@ -37,16 +38,11 @@ fn main() -> io::Result<()> {
     }
 
     {
-        let pusher = accumulator.clone();
-
-        let sink = Sink::new(pusher);
-
-        sink.start();
+        let sink = Sink::new(accumulator.clone());
+        let _ = sink.start().expect(SINK_START_EXPECT);
 
         let interval = Duration::from_secs(RUNNABLE_CHECK_INTERVAL_SECONDS);
-
         let runnable = get_sigint_handle();
-
         while runnable.load(Ordering::SeqCst) {
             std::thread::park_timeout(interval);
         }
